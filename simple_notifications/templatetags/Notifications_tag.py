65a9a83baa168f8f236b82e648b414e5b05da2f4
Alireza Molaee <alirezamolaii@gmail.com>
2015-12-14 11:02:30 +0330

    Add templatetags it is contain 1 template tag and 1 filter notifications_link: for render notification list icon_type: to get icon type; for example: fa-error type is fa Add notification template

---
+++ simple_notifications/templatetags/Notifications_tag.py
@@ -0,0 +1,36 @@
+from django.template import Library
+from simple_notifications.models import Notification
+from django.template.defaultfilters import stringfilter
+
+register = Library()
+
+
+@register.inclusion_tag('notification/nav_link.html')
+def notifications_link(user, limit=None):
+    all_count = Notification.objects.count()
+    not_seen_count = Notification.objects.not_seen(user)
+    if limit is None:
+        notifications = Notification.objects.filter(
+            user=user
+        )
+    else:
+        notifications = Notification.objects.filter(
+            user=user
+        )[:limit]
+    notifications_list = []
+    for n in notifications:
+        notifications_dic = {
+            'icon': n.icon,
+            'color': n.color,
+            'content': n.content,
+            'seen_date': n.seen_date,
+            'url': n.url}
+        notifications_list.append(notifications_dic)
+    return {'notifications': notifications_list, 'all_count': all_count,
+            'not_seen_count': not_seen_count}
+
+
+@register.filter(name='icon_type')
+@stringfilter
+def icon_type(value):
+    return value.split('-')[0]
