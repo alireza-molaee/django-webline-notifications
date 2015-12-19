18ccd2fea5baf9cafd809338406891932f9101bf
Alireza Molaee <alirezamolaii@gmail.com>
2015-12-19 18:43:53 +0330

    Add see_all_notification view

--- simple_notifications/views.py
+++ simple_notifications/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from models import Notification
+from django.http import HttpResponse
 
-# Create your views here.
+
+@login_required
+def see_all_notification(request):
+    user = request.user
+    Notification.seen_all(user)
+    html = ("<h2>see notifications</h2><br>cheek seen for %s" % user.get_full_name)
+    return HttpResponse(html)
