6bb6e0576b2a544b84b720ebd21b4b562dcff56a
Alireza Molaee <alirezamolaii@gmail.com>
2015-12-21 11:17:49 +0330

    Distinguish between superuser and other

--- simple_notifications/admin.py
+++ simple_notifications/admin.py
@@ -5,8 +5,34 @@ from models import Notification
 class NotificationAdmin(admin.ModelAdmin):
     raw_id_fields = ["user"]
     list_filter = ["color", "send_date"]
-    list_display = ["user", "get_icon_html", "seen_date", "send_date",
-                    "get_link"]
     search_fields = ["user"]
+
+    def get_queryset(self, request):
+        qs = super(NotificationAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
     
-admin.site.register(Notification, NotificationAdmin) #TODO: add it to setting
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return ["user", "get_icon_html", "seen_date", "send_date",
+                    "get_link"]
+        else:
+            return ["get_icon_html", "seen_date", "send_date", "get_link"]
+        
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []
+        else:
+            return ["user", "icon", "color", "content", "url", "seen_date",
+                    "send_date"]
+    
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        else:
+            return False
+      
+            
+    
+admin.site.register(Notification, NotificationAdmin)
