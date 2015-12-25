313cf1b59c2bcbb1db5066d23e22767dea1f4941
Alireza Molaee <alirezamolaii@gmail.com>
2015-12-25 16:01:39 +0330

    Update test to contain more ability

--- simple_notifications/tests.py
+++ simple_notifications/tests.py
@@ -9,45 +9,153 @@ class NotificationsTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         admin = User.objects.get(pk=1)
-        students = User.objects.filter(groups__name='student')
-        arc = User.objects.get_by_natural_key('arc')
+        group_foo = User.objects.filter(groups__name='group_foo')
+        staff = User.objects.get_by_natural_key('staff')
+        cls.admin_user = admin
         cls.n2_admin = Notification.send(
             [admin],
             'test notification to admin',
-            'fa-error',
+            'fa-info',
             Notification.COLOR_DANGER,
             url='http://www.google.com/'
             )
-        cls.n2_arc = Notification.send(
-            [arc],
-            'test notifications to arc',
-            'fa-alert',
+        cls.n2_staff = Notification.send(
+            [staff],
+            'test notifications to staff',
+            'fa-bell',
             Notification.COLOR_DANGER,
         )
-        cls.n2_students = Notification.send(
-            students,
-            'test notifications to students',
-            'fa-alert',
+        cls.n2_group_foo = Notification.send(
+            group_foo,
+            'test notifications to client',
+            'fa-bell',
             Notification.COLOR_WARNING,
             ''
         )
         
     def test_icon_str(self):
-        icon_str = self.n2_admin[0].icon
+        icon_str1 = self.n2_admin[0].icon
+        icon_str2 = self.n2_staff[0].icon
+        icon_str3 = self.n2_group_foo[0].icon
         self.assertEqual(
-            icon_str,
-            'fa-error')
+            icon_str1,
+            'fa-info'
+        )
+
+        self.assertEqual(
+            icon_str2,
+            'fa-bell'
+        )
+
+        self.assertEqual(
+            icon_str3,
+            'fa-bell'
+        )
 
     def test_icon_html(self):
-        icon_html = self.n2_admin[0].get_icon_html()
+        icon_html1 = self.n2_admin[0].get_icon_html()
+        icon_html2 = self.n2_staff[0].get_icon_html()
+        icon_html3 = self.n2_group_foo[0].get_icon_html()
         self.assertEqual(
-            icon_html,
-            '<i class="fa fa-error" style="color:#f56954;"></i>'
+            icon_html1,
+            '<i class="fa fa-info" style="color:#f56954;width:100%;text-align:center;"></i>'
+        )
+
+        self.assertEqual(
+            icon_html2,
+            '<i class="fa fa-bell" style="color:#f56954;width:100%;text-align:center;"></i>'
+        )
+
+        self.assertEqual(
+            icon_html3,
+            '<i class="fa fa-bell" style="color:#f39c12;width:100%;text-align:center;"></i>'
         )
     
     def test_get_link(self):
-        link = self.n2_admin[0].get_link()
+        link1 = self.n2_admin[0].get_link()
+        link2 = self.n2_staff[0].get_link()
+        link3 = self.n2_group_foo[0].get_link()
         self.assertEqual(
-            link,
-            '<a href="http://www.google.com/"><i class="fa fa-link"></i>Link</a>'
+            link1,
+            '<a href="http://www.google.com/"><i class="fa fa-link"></i>&nbspLink</a>'
+        )
+        self.assertEqual(
+            link2,
+            None
+        )
+        self.assertEqual(
+            link3,
+            None
+        )
+        
+    def test_seen(self):
+        seen_befor = self.n2_admin[0].seen
+        self.assertEqual(
+            seen_befor,
+            False
+        )
+        
+        Notification.seen_all(self.admin_user)
+        seen_after = Notification.objects.get(pk=self.n2_admin[0].pk).seen
+        self.assertEqual(
+            seen_after,
+            True
+        )
+        
+    def test_limit_notification(self):
+        Notification.objects.get(pk=self.n2_admin[0].pk).delete()
+        for i in range(8):
+            Notification.send(
+                [self.admin_user],
+                'test {}'.format(i),
+                'fa-info',
+                Notification.COLOR_DANGER,
+                url='http://www.google.com/'
+            )
+        with self.settings(SIMPLE_NOTIFICATIONS_LIMIT=5):
+            Notification.objects.filter(
+                user=self.admin_user
+            )[0].limit_notification()
+            all_n_after = Notification.objects.filter(user=self.admin_user)
+            count = all_n_after.count()
+            first_content = all_n_after.order_by('send_date')[0].content
+            last_content = all_n_after.order_by('-send_date')[0].content
+            self.assertEqual(
+                count,
+                5
+            )
+            self.assertEqual(
+                first_content,
+                'test 3'
+            )
+            self.assertEqual(
+                last_content,
+                'test 7'
+            )
+        with self.settings(SIMPLE_NOTIFICATIONS_LIMIT=False):
+            Notification.objects.filter(
+                user=self.admin_user
+            )[0].limit_notification()
+            all_n_after = Notification.objects.filter(user=self.admin_user)
+            count = all_n_after.count()
+            first_content = all_n_after.order_by('send_date')[0].content
+            last_content = all_n_after.order_by('-send_date')[0].content
+            self.assertEqual(
+                count,
+                5
+            )
+            
+    def test_see_view(self):
+        self.assertEqual(
+            self.client.get('/en-us/notifications/see-all/').status_code,
+            302
+        )
+        self.client.login(username='admin', password='123')
+        self.assertEqual(
+            self.client.get('/en-us/notifications/see-all/').status_code,
+            403
+        )
+        self.assertEqual(
+            self.client.get('/en-us/notifications/see-all/', HTTP_X_REQUESTED_WITH='XMLHttpRequest').status_code,
+            200
         )
