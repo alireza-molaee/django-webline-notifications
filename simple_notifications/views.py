ff2c88bdc04c20ea834278c5427fd92b00288b19
Alireza Molaee <alirezamolaii@gmail.com>
2015-12-25 16:00:34 +0330

    Improve see_all_notification view

--- simple_notifications/views.py
+++ simple_notifications/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from models import Notification
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 
 
 @login_required
 def see_all_notification(request):
-    user = request.user
-    Notification.seen_all(user)
-    html = ("<h2>see notifications</h2><br>cheek seen for %s" % user.get_full_name)
-    return HttpResponse(html)
+    if request.is_ajax():
+        Notification.seen_all(request.user)
+        return HttpResponse()
+    else:
+        return HttpResponseForbidden('<h1>Forbidden</h1>you cant use this url in this way')
